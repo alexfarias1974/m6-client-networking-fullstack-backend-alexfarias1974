dec5c52bcb2605c66cd3c67d80bd6e8199d5d205
alexfarias1974 <alex_wfariaas@yahoo.com.br>
2023-02-09 09:33:11 -0400

    fix: cors config

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
 import "reflect-metadata"
 import "express-async-errors"
+import cors from "cors"
 import express from "express"
-import "express-async-errors"
 import routes from "./routes/user.routes"
 import userRoutes from "./routes/user.routes"
-import handleErrorMiddleware from "./middlewares/handleError.middleware"
 import sessionRoutes from "./routes/session.routes"
 import contactRoutes from "./routes/contact.routes"
+import handleErrorMiddleware from "./middlewares/handleError.middleware"
+
 
 const app = express()
 app.use(express.json())
 
-var cors = require("cors");
 app.use(cors());
 
 app.use("/login", sessionRoutes)
